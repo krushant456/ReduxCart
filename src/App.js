0920c89169a87648d066c66540c677f6e7cee656
krushant_koshti_ <136622399+krushant456@users.noreply.github.com>
2023-11-12 12:56:02 +0530

    Adding Files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import { useSelector } from 'react-redux';
+function App() {
+
+
+  const showcart = useSelector((state) =>state.ui.cartISVisible
+  )
+  return (
+    <Layout>
+      {
+        showcart || <Cart />
+      }
+
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/Shop/Products.js
@@ -0,0 +1,41 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+
+
+const DUMMY_PRODUCT = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My first Book',
+    description: 'the first book I ever wrote'
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: 'the second book I ever wrote'
+  }
+]
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCT.map((product) => (
+
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
